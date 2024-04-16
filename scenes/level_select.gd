040f4e06cd89ac24cf6c063cd88dadf8981cc573
Matthew <matttz@att.net>
2024-04-16 12:11:16 -0500

    Button Changes

--- scenes/level_select.gd
+++ scenes/level_select.gd
@@ -2,18 +2,18 @@ extends Control
 
 
 
-func _on_to_calculus_pressed():
+func _on_ToCalculus_pressed():
 	while $Path2D/William.progress_ratio < 1 :
 		$Path2D/William.progress_ratio += .001
 
 
-func _on_to_student_pressed():
+func _on_ToStudent_pressed():
 	pass # Replace with function body.
 
 
-func _on_to_comp_sci_pressed():
+func _on_ToCompsci_pressed():
 	pass # Replace with function body.
 
 
-func _on_to_music_pressed():
+func _on_ToMusic_pressed():
 	pass # Replace with function body.
