98dbd8668f21ca26404d6dcb270acaef6e377f72
Matthew <matttz@att.net>
2024-04-17 12:11:15 -0500

    Level Select Fixes
    jack did stuff

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,21 +1,59 @@
 extends Node
-class_name Character
 
-@export var games: Array[PackedScene]
+signal done(win: bool)
 
-var encounters = []
+@export_category("Games")
+@export
+var games: Array[PackedScene]
+@export
+var boss: PackedScene
 
-var met_character = false
+@export_category("Game Flow Modifiers")
+@export
+var gamesToBoss: int
+@export
+# Speed added to games after each interval of gamesToSpeedUp
+var speedIncrement: float
+@export
+# Number of games that must be played before a speed increase. Default speed modifier is one.
+var gamesToSpeedUp: int
 
-signal gameover(win: bool)
 
-func on_start():
-	encounters.resize(games.size())
-	encounters.fill(0)
- 
-func load_state(state):
-	if not state["Encounters"] == null:
-		encounters = state["Encounters"]
+var lives: int
+var gamesPlayed: int
 
-	if not state["MetCharacter"] == null:
-		met_character = state["MetCharacter"]
+func start():
+	lives = 4
+	gamesPlayed = 0
+	
+	var game = games.pick_random()
+	var game_i = game.instantiate()
+	game_i.name = "Current"
+	game_i.gameover.connect(_on_gameover)
+	add_child(game_i)
+
+func _on_gameover(win: bool):
+	get_node("Current").queue_free()
+	
+	if not win:
+		lives -= 1
+		
+	gamesPlayed += 1
+	if gamesPlayed == gamesToBoss:
+		var boss_i = boss.instantiate()
+		boss_i.gameover.connect(_on_boss_gameover)
+		add_child(boss_i)
+		boss_i.start(1.0)
+		return
+		
+	var game = games.pick_random()
+	var game_i = game.instantiate()
+	game_i.name = "Current"
+	game_i.gameover.connect(_on_gameover)
+	add_child(game_i)
+	game_i.start(1.0 + speedIncrement*(gamesPlayed%gamesToSpeedUp))
+
+func _on_boss_gameover(win: bool):
+	get_node("Current").queue_free()
+	
+	done.emit(win)
