9b4487cde3b3182d3d4f545f5a72602b9c3e64cd
Jack Saenz <jack.p.saenz@gmail.com>
2024-04-22 12:32:17 -0500

    Created Timer + new minigame

---
+++ scripts/timer.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+signal done
+
+func _ready():
+	start(5.0)
+
+func start(time: float):
+	$Explosion.visible = false
+	$PencilSharpener.visible = true
+	$Timer.wait_time = time
+	$Timer.start()
+	
+	$PencilSharpener/ShavingParticles.emitting = true
+
+func _process(delta):
+	$Box/Pencil.position.y = 450 - (1 - ($Timer.time_left / $Timer.wait_time))*975
+	
+	if position.y <= -330 && $PencilSharpener/ShavingParticles.emitting:
+		$PencilSharpener/ShavingParticles.emitting = false
+
+func _on_timer_timeout():
+	done.emit()
+	$PencilSharpener.visible = false
+	$Explosion.visible = true
+	$PencilSharpener/ShavingParticles.emitting = false
