f192142a516e65a98be743b2e9fd961cdf63de42
JoshuaCHolmes <joshua.christopher.holmes@gmail.com>
2024-05-07 12:26:48 -0500

    Typing Game and character script fixes

--- scripts/character.gd
+++ scripts/character.gd
@@ -21,39 +21,55 @@ var gamesToSpeedUp: int
 
 var lives: int
 var gamesPlayed: int
+var speedIncrementCount: int
+var game_i
+var boss_i
 
 func start():
 	lives = 4
 	gamesPlayed = 0
+	gamesToSpeedUp = 3
+	gamesToBoss = 10
+	speedIncrement = 0.5
+	speedIncrementCount = 0
 	
 	var game = games.pick_random()
-	var game_i = game.instantiate()
+	game_i = game.instantiate()
 	game_i.name = "Current"
 	game_i.gameover.connect(_on_gameover)
 	add_child(game_i)
+	game_i.start(1.0/(1+speedIncrement*(gamesPlayed%gamesToSpeedUp)))
 
 func _on_gameover(win: bool):
-	get_node("Current").queue_free()
+	game_i.queue_free()
+
+	gamesPlayed += 1
 	
 	if not win:
 		lives -= 1
+	
+	if lives > 0:
+		if gamesPlayed == gamesToBoss:
+			boss_i = boss.instantiate()
+			boss_i.gameover.connect(_on_boss_gameover)
+			add_child(boss_i)
+			boss_i.start(1.0)
+			return
 		
-	gamesPlayed += 1
-	if gamesPlayed == gamesToBoss:
-		var boss_i = boss.instantiate()
-		boss_i.gameover.connect(_on_boss_gameover)
-		add_child(boss_i)
-		boss_i.start(1.0)
-		return
+		var game = games.pick_random()
+		game_i = game.instantiate()
+		game_i.name = "Current"
+		game_i.gameover.connect(_on_gameover)
+		add_child(game_i)
 		
-	var game = games.pick_random()
-	var game_i = game.instantiate()
-	game_i.name = "Current"
-	game_i.gameover.connect(_on_gameover)
-	add_child(game_i)
-	game_i.start(1.0 + speedIncrement*(gamesPlayed%gamesToSpeedUp))
+		if gamesPlayed % gamesToSpeedUp == 0:
+			speedIncrementCount += 1
+	
+		game_i.start(1.0/(1+speedIncrement*(speedIncrementCount)))
+	else:
+		done.emit(false)
 
 func _on_boss_gameover(win: bool):
-	get_node("Current").queue_free()
+	boss_i.queue_free()
 	
 	done.emit(win)
